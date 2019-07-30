03f9206d2b06475006217733b3c369113edb5dd5
Natalia Helińska <natalia.w.helinska@gmail.com>
2019-07-30 18:19:43 +0200

    New stuff

---
+++ Database1/Database1/dbo/Tables/denormTrack.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[denormTrack] (
+    [TrackId]       INT             NOT NULL,
+    [TrackName]     NVARCHAR (200)  NOT NULL,
+    [AlbumId]       INT             NULL,
+    [Composer]      NVARCHAR (220)  NULL,
+    [Milliseconds]  INT             NOT NULL,
+    [Bytes]         INT             NULL,
+    [UnitPrice]     NUMERIC (10, 2) NOT NULL,
+    [GenreName]     NVARCHAR (120)  NULL,
+    [MediaTypeName] NVARCHAR (120)  NULL
+);
+
---
+++ Database1/Database1/dbo/Views/v_Track.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[v_Track]
+	AS
+	SELECT 
+		[tr].[TrackId]
+	,	[tr].[Name]				AS [TrackName]
+	,	[tr].[AlbumId]
+	,	[tr].[Composer]
+	,	[tr].[Milliseconds]
+	,	[tr].[Bytes]
+	,	[tr].[UnitPrice]
+	,	[gr].[Name]				AS [GenreName]
+	,	[mt].[Name]				AS [MediaTypeName]
+	FROM 
+			[dbo].[Track]	AS tr
+	INNER JOIN	[dbo].[MediaType]	AS mt ON [mt].[MediaTypeId] 	= [tr].[MediaTypeId]
+	INNER JOIN	[dbo].[Genre]	AS gr ON [gr].[GenreId]		= [tr].[GenreId]
